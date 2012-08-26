3e848d6cc67b11b67e7b08760bc2f507aeda6abb
Marc Juchli <marc@mjnet.ch>
2012-08-26 15:27:14 +0200

    Added parameter function for vzdump command. See also git links

--- vzdump_s3.sh
+++ vzdump_s3.sh
@@ -6,27 +6,29 @@
 #         USAGE:  bash vzdump_s3.sh
 #
 #   DESCRIPTION:  vzdump_s3 is a bash script that handles your 
-#		  OpenVZ backup and sync it with Amazon S3
+#		   OpenVZ backup and sync it with Amazon S3
 #
 #       OPTIONS:  See configuration tag
 #  REQUIREMENTS:  s3cmd, vzdump
-#          BUGS:  ---
+#          BUGS:  https://github.com/mjnet/vzdump_s3/issues
 #         NOTES:  ---
 #        AUTHOR:  Marc Juchli
 #       COMPANY:  Codag GmbH
-#       VERSION:  1.0
+#       VERSION:  1.0.1
 #       CREATED:  25/08/12 17:46:08 CEST
-#      REVISION:  ---
+#      REVISION:  https://github.com/mjnet/vzdump_s3
 #=======================================================================
 
 #-----------------------------------------------------------------------
 # CONFIGURATION
 #-----------------------------------------------------------------------
-SERVER="102" #USE EITHER SERVER "VID" OR "--all"
+SERVER="201" #USE EITHER SERVER "VID" OR "--all"
 
 BACKUP_NAME="daily" #NAME OF THE BACKUP JOB
 BACKUP_COUNT=5 #NUMBER OF BACKUPS TO KEEP
 BACKUP_ROOT="/home/backup/vz/" #BACKUP DIRECTORY (USE "/" IN THE END)
+BACKUP_PARAMETER="--suspend --compress --bwlimit 51200"
+BACKUP_BZIP2=0
 
 BUCKET_NAME=`hostname` #S3 BUCKET NAME
 
@@ -48,7 +50,7 @@ cd $BACKUP_FOLDER/
 #-----------------------------------------------------------------------
 # MAKE VZDUMP IN VID RELATED FOLDER OR GLOBAL FOLDER FOR --all
 #-----------------------------------------------------------------------
-vzdump --suspend --dumpdir $BACKUP_FOLDER $SERVER
+vzdump $BACKUP_PARAMETER --dumpdir $BACKUP_FOLDER $SERVER
 
 #-----------------------------------------------------------------------
 # CHECK IF BUCKET ALREADY EXISTS
@@ -80,7 +82,9 @@ fi
 #-----------------------------------------------------------------------
 # INSTEAD OF USE --compress IN VZDUMP WE MAKE bzip2 NOW SAVING ABOUT 10%
 #-----------------------------------------------------------------------
-bzip2 *.tar
+if [ $(( BACKUP_BZIP2 )) -eq 1 ]; then
+	bzip2 *.tar
+fi
 
 #-----------------------------------------------------------------------
 # AT LAST SYNC WITH S3 BUCKET
